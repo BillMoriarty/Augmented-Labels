b74e7f6cbc532ced65a001f2f53ab845875485a1
Bill Moriarty <wrmoriarty@gmail.com>
2022-08-22 19:32:46 -0400

    Initial Commit

---
+++ Augmented Labels/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Augmented Labels
+//
+//  Created by Bill Moriarty on 8/22/22.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
